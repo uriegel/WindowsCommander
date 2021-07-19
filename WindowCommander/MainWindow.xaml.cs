2e67b9628231d53fb195a515d81c7a63bbca7dd1
Uwe Riegel <uriegel@hotmail.de>
2021-07-19 14:45:52 +0200

    Custom Window Titlebar

--- WindowCommander/MainWindow.xaml.cs
+++ WindowCommander/MainWindow.xaml.cs
@@ -1,28 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PInvoke;
+
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Interop;
 
 namespace WindowCommander
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window
 	{
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
+		public MainWindow() => InitializeComponent();
+
+		void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+			=> User32.PostMessage(windowHandle, User32.WindowMessage.WM_NCLBUTTONDOWN, new IntPtr(2), IntPtr.Zero);
+
+		void Window_Loaded(object sender, RoutedEventArgs e) => windowHandle = new WindowInteropHelper(this).Handle;
+
+		IntPtr windowHandle;
 	}
 }
+
+// TODO Hover in Window Control Buttons
+// TODO Click in Window Control Buttons
+// TODO Icon
+// TODO Dblclk in Title
+// TODO Save Windows State
