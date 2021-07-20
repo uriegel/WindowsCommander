48e69b0aae5ebbbb698463251c50b8a06f21aae8
Uwe Riegel <uriegel@hotmail.de>
2021-07-20 10:15:38 +0200

    Window Controls, Dblclk in Title

--- WindowsCommander/MainWindow.xaml.cs
+++ WindowsCommander/MainWindow.xaml.cs
@@ -11,17 +11,33 @@ namespace WindowCommander
 	{
 		public MainWindow() => InitializeComponent();
 
-		void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-			=> User32.PostMessage(windowHandle, User32.WindowMessage.WM_NCLBUTTONDOWN, new IntPtr(2), IntPtr.Zero);
+		void Grid_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (e.MouseDevice.LeftButton == MouseButtonState.Pressed)
+				User32.PostMessage(windowHandle, User32.WindowMessage.WM_NCLBUTTONDOWN, new IntPtr(2), IntPtr.Zero);
+		}
+
+		void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ClickCount == 2)
+				Maximize_Click(this, e);
+		}
 
 		void Window_Loaded(object sender, RoutedEventArgs e) => windowHandle = new WindowInteropHelper(this).Handle;
+
+		void Minimize_Click(object sender, MouseButtonEventArgs e)
+			=> WindowState = WindowState.Minimized;
+		void Maximize_Click(object sender, MouseButtonEventArgs e)
+			=> WindowState = WindowState == WindowState.Normal
+				? WindowState.Maximized
+				: WindowState.Normal;
+
+		void Close_Click(object sender, MouseButtonEventArgs e)
+			=> Close();
 
 		IntPtr windowHandle;
 	}
 }
 
-// TODO Hover in Window Control Buttons
-// TODO Click in Window Control Buttons
 // TODO Icon
-// TODO Dblclk in Title
 // TODO Save Windows State
