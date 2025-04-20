14d801002681e904d73a7214aa913e16ada07e21
Uwe Riegel <uriegel@hotmail.de>
2025-04-20 11:07:29 +0200

    Window bounds are being saved

--- Commander/Views/MainWindow.xaml.cs
+++ Commander/Views/MainWindow.xaml.cs
@@ -8,4 +8,34 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
+
+    protected override void OnSourceInitialized(EventArgs e)
+    {
+        base.OnSourceInitialized(e);
+
+        // Restore bounds
+        if (Properties.Settings.Default.WindowWidth > 0 &&
+            Properties.Settings.Default.WindowHeight > 0)
+        {
+            Top = Properties.Settings.Default.WindowTop;
+            Left = Properties.Settings.Default.WindowLeft;
+            Width = Properties.Settings.Default.WindowWidth;
+            Height = Properties.Settings.Default.WindowHeight;
+        }
+
+        // Restore window state after layout is applied
+        this.Loaded += (s, ev) => WindowState = Properties.Settings.Default.WindowState;
+    }
+
+    protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+    {
+        Properties.Settings.Default.WindowTop = Top;
+        Properties.Settings.Default.WindowLeft = Left;
+        Properties.Settings.Default.WindowWidth = Width;
+        Properties.Settings.Default.WindowHeight = Height;
+        Properties.Settings.Default.WindowState = WindowState;
+
+        Properties.Settings.Default.Save();
+        base.OnClosing(e);
+    }
 }
