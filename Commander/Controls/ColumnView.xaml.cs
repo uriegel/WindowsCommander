adfd493a08f74bc44e88dd2ceb71cdc870a46eef
Uwe Riegel <uriegel@hotmail.de>
2025-04-21 09:12:24 +0200

    TODO

--- Commander/Controls/ColumnView.xaml.cs
+++ Commander/Controls/ColumnView.xaml.cs
@@ -9,3 +9,12 @@ public partial class ColumnView : UserControl
         InitializeComponent();
     }
 }
+
+// TODO Modern UI Menu with Actions
+// TODO In TestView controlling Selections via Keybord: Ins, Ctrl+A, Shift End, Shift Home
+// TODO Don't select parent item
+// TODO HeaderControl with a variable number of Headers
+// TODO HeaderControl to TestView
+// TODO Resize HeaderColumns
+// TODO ColumnView with HeaderControl and Controller
+// TODO Controller with DataContext, HeaderControl
